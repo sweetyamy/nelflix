c3b831e3de658e97b923afeb66fe8cc8ee4077fa
Amy Ahn <amy.ahn.kr@gmail.com>
2024-09-02 17:43:49 -0400

    minor change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log('API Key:', API_KEY);
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3', // version 3
